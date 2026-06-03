2d57d1a58a83967bb701d120c0d55827e9e63c14
sully19nv <canosulley@gmail.com>
2026-06-03 17:06:07 +0000

    + actualiza modificaciones app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const btmMensaje  = document.getElementById('btmMensaje')
+const btnMensaje = document.getElementById('btnMensaje')
 const mensajeClase = document.getElementById('mensajeClase')
-btmMensaje.addEventListener('click',()=>{
-    mensajeClase.textContent = '¡Hola! Este es un mensaje de alerta.'
-    mensajeClase.textContent = 'el texto que quisimos mostrar'
+
+btnMensaje.addEventListener('click', () => {
+mensajeClase.textContent = '¡Hola! Este es un mensaje de alerta.'
 })
