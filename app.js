dd0d33a2259757e9a4a606bf6bb58279e3251de2
sully19nv <canosulley@gmail.com>
2026-06-03 19:52:42 +0000

    +registrar todos los cambios realizados durante la clase-28-pantalla-candidato

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const btnMensaje = document.getElementById('btnMensaje')
 const mensajeClase = document.getElementById('mensajeClase')
+const botonesperfil = document.querySelectorAll('.btnPerfil')
+const detallePerfil = document.getElementById('detallePerfil')
 
 let contadorDeClicks = 0
 
@@ -10,4 +12,11 @@ mensajeClase.textContent =
 `El texto que quisimos mostrar. Número de clics: ${contadorDeClicks}`
 
 
+})
+
+botonesperfil.forEach(boton => {
+    boton.addEventListener('click',() => {
+     const perfil = boton.getAttribute('data-perfil')
+     detallePerfil.innerHTML = `<h3>${perfil}</h3><p>Información sobre el perfil ${perfil}.</p>`
+    })
 })
