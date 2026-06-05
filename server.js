61dc9221ad01a3b5bc5d88b4682ce5d64291a4eb
sully19nv <canosulley@gmail.com>
2026-06-05 01:15:16 +0000

    agrega y registra y actualiza los cambios realizados en server.js

--- server.js
+++ server.js
@@ -1,20 +1,15 @@
 import express from "express";
 import cors from "cors";
-import path from "path";
-import { fileURLToPath } from "url";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-app.use(express.static(__dirname));
+app.use(express.static("."));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(process.cwd() + "/index.html");
 });
 
 const PORT = 3000;
